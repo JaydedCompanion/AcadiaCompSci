d0813d2f1f63aba4ab54416fb8d59ce469ef96aa
juan callejas <TheShadyColombian@dyna220-234.acadiau.ca>
2017-10-18 12:12:59 -0400

    Fixed Assignment 2
    -Made rgb array use .append() instead, as for some stupid reason it was
    making the array size 1 no matter what i declared it as
    -Removed parameter ‘array’ and used r instead
    -Made the getMin() method check if the first parameter is a color
    -Removed redundant debugging print() calls, as it was spamming the
    terminal and slowing down the program

--- Assignment #2/a2-1113-100143996.py
+++ Assignment #2/a2-1113-100143996.py
@@ -12,8 +12,6 @@ def changePixelGrey (pix):
 	
 	value = int (getMin (pix.getColor()))
 	
-	print value
-	
 	changePixelColor (pix, makeColor (value, value, value))
 	
 	return pix
@@ -43,18 +41,18 @@ def everySecond (pic):
 #Find the smallest value of an array,
 #An alternate mode is available, where individual rgb values may be passed and an array will be generated and used
 #Yet another altername mode is available where a color object is passed, and it is called again but with those values as parameters instead
-def getMin (array = None, r = None, g = None, b = None):	
+def getMin (r = None, g = None, b = None):	
 	
 	#If we're using rgb parameters use this code
-	if (not r == None) and (not g == None) and (not b == None):
+	if (not r == None) and (not g == None) and (not b == None) and (not isinstance (r, list)):
 		
-		print "Getting smallest value in rgb: " + str (r) + ", " + str (g) + ", " + str (b)
+		#print "Getting smallest value in rgb: " + str (r) + ", " + str (g) + ", " + str (b)
 		
 		#Create an array out of the rgb parameters
-		rbg = [3]
-		rgb [0] = r
-		rgb [1] = g
-		rgb [2] = b
+		rgb = []
+		rgb.append (r)
+		rgb.append (g)
+		rgb.append (b)
 		
 		#Is this technically recusion?
 		#Call this method again, but pass the rgb array as a parameter instead
@@ -62,22 +60,23 @@ def getMin (array = None, r = None, g = None, b = None):
 		
 		
 	#Why the hell is '!=' not a thing in python
-	#If we're using the array parameter use this code instead
+	#If we're using only one parameter, do this instead
 	
-	if not array == None:
-
+	elif (not r == None) and (g == None) and (b == None):
+		
+		if isinstance (r, java.awt.Color):
+			return getMin (r.getRed(), r.getGreen(), r.getBlue())
+		
 		#Do not proceed if something other than an array with at least 1 element is passed
-		if not isinstance (array, list) or len (array) < 1:
+		if not isinstance (r, list) or len (r) < 1:
 			print "ERROR: parameter \"array\" is not an array with at least 1 value"
-			print "Getting smallest value in colour " + str (array)
-			return getMin (array.getRed(), array.getGreen(), array.getBlue())
 		
-		print "Getting smallest value in array " + str (array)
-		
-		minimum = array [0];
-		for i in range (0, len (array)):
-			if (array[i] < minimum):
-				minimum = array [i]
+		minimum = r [0];
+		for i in range (0, len (r)):
+			if (r[i] < minimum):
+				minimum = r [i]
 	
 	#Return the lowest minimum value
 	return minimum
+
+explore (greyOut (makePicture (pickAFile())))
