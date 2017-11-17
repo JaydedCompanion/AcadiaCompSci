252189c905e30418f86f07747b1d991f7b951c8f
Juan Callejas <jcallejasrodriguez@gmail.com>
2017-11-17 09:56:56 -0400

    Assignment #5 - Completed Question 2

--- Assignment #5/a5-11130100143996.py
+++ Assignment #5/a5-11130100143996.py
@@ -22,3 +22,49 @@ def step (sound, cut):
 		i += 1
 	
 	return sound
+
+#QUESTION 2
+def stats (sound):
+    
+    #Store first sample in individual variable for easy access
+    firstSample = getSamples (sound) [0]
+    
+    #Define variables in broader scope so they aren't reset during each loop
+    zeroCount = 0
+    largestValue = getSampleValue (firstSample)
+    largestCount = 0
+    smallestValue = largestValue
+    smallestCount = 0
+    
+    #Loop through every sample in the sound
+    for sample in getSamples (sound):
+        
+        #Store the sample value in individual variable for easy access
+        val = getSampleValue (sample)
+        
+        #Check for zero-value
+        if (val == 0):
+            zeroCount += 1
+        
+        #Check if the sample value matches the smallest or largest value
+        if (val == largestValue):
+            largestCount += 1
+        if (val == smallestValue):
+            smallestCount += 1
+        
+        #If the current sample value is greater than the existing max value
+        #reset the largest count and increase the largestValue to the new max
+        #and do the same with the smallestValue and smallestCount variables
+        if (val > largestValue):
+            largestCount = 1    #Reset to 1 instead of 0 since technically the value was found/exists at least once
+            largestValue = val
+            print ("New max found. Clearing counter")
+        if (val < smallestValue):
+            smallestCount = 1   #Reset to 1 instead of 0 since technically the value was found/exists at least once
+            smallestValue = val
+            print ("New min found. Clearing counter")
+    
+    print ("The largest value (" + str(largestValue) + ") occurs " + str(largestCount) + " times.")
+    print ("The smallest value (" + str(smallestValue) + ") occurs " + str(smallestCount) + " times.")
+    print ("There are " + str(zeroCount) + " zeros.")
+    
