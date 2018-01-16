f78abb8d816f4b8e62f7ae3157f056ab053a065d
Juan Callejas <jcallejasrodriguez@gmail.com>
2018-01-16 15:50:09 -0400

    Assignment #1 - Completed Question 1

---
+++ Year 1 - Semester 2/src/Tree.java
@@ -0,0 +1,27 @@
+//*************************************************************************************************
+//
+// 	Tree.java				Author: Juan Sebastian Callejas Rodriguez		ID:100143996
+//
+//	This program prints out a tree outline using asterisks
+//
+// *************************************************************************************************
+
+public class Tree {
+
+	public static void Tree () {
+
+		System.out.println ("             -					");
+		System.out.println ("           _/ \\_				");
+		System.out.println ("          \\     /				");
+		System.out.println ("          /_'O'_\\				");
+		System.out.println ("           _O_'_				");
+		System.out.println ("          _'_'O'_				");
+		System.out.println (" Happy   O'_O_'_O_  Holidays	");
+		System.out.println ("        _'_O_'_'_O_			");
+		System.out.println ("       _O_'_O_'_O_'_			");
+		System.out.println ("          [_____]				");
+		System.out.println ("           \\___/				");
+
+	}
+
+}
