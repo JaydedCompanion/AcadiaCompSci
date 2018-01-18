de5c8558c53aa30d57c905630ae4a8f2f6895ce1
Juan Callejas <jcallejasrodriguez@gmail.com>
2018-01-18 03:08:30 -0400

    Lab #1 - Completed

---
+++ Year 1 - Semester 2/src/Lab1/Lab1.java
@@ -0,0 +1,19 @@
+package Lab1;
+
+public class Lab1 {
+	
+	private static String name;
+	private static int age;
+	private static int years;
+	
+	public static void main (String[] params) {
+		
+		name = "Juan Callejas";
+		age = 18;
+		years = age + 10;
+		
+		System.out.println("Your name is " + name + "\nYour age is " + age + "\nYour age in 10 years will be " + years);
+		
+	}
+	
+}
